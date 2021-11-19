a058dfbb5f0e15ae3f5af71ed42b49b300a42abc
Alejandro Garcia <alejandro.garcia@students.fhnw.ch>
2021-11-19 09:45:25 -0500

    init project with welcome comp. sample chart comp.

---
+++ app/src/components/ChartOne.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-col align="center" sm="12">
+    <h2>{{ title }}</h2>
+    <v-card :height="this.$vuetify.breakpoint.height * 0.7" :width="this.$vuetify.breakpoint.width * 0.8" class="rounded-xl" elevation="5">
+      <v-row justify="center" align-content="center" class="mt-5">
+        <v-col>
+          <p align="center"><strong>BTC</strong></p>
+
+        </v-col>
+      </v-row>
+
+
+
+
+    </v-card>
+  </v-col>
+
+</template>
+
+<script>
+export default {
+  name: "ChartOne",
+  props: ['title',],
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-col align="center">
+    <h1 class="indigo--text" style="text-align: center">{{ text }}</h1>
+    <v-btn @click="startFunction" color="indigo" dark class="align-center mt-5" x-large>Start</v-btn>
+  </v-col>
+
+
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  props: ['text', 'startFunction'],
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
